8a60d43edf5ce0e2d86bc28afd5a34c95ceb669c
Ushen Wijayaratne <108585387+its-ushen@users.noreply.github.com>
2023-07-17 09:37:14 +0100

    Update scrolldown.js
    commented the file

--- js/scrolldown.js
+++ js/scrolldown.js
@@ -1,17 +1,20 @@
-// const body = document.body;
-// let lastScroll = 0;
+// how does this code work? 
 
+// gather the hero element 
 const hero = document.querySelector('.hero');
 console.log(hero);
 
+// add an event listener to the window
 window.addEventListener("scroll", () => {
+	// create a variable to store the number of pixels scrolled off the top of the page.
 	const currentScroll = window.scrollY;
-    console.log(currentScroll);
+	// if it's greater than zero activate the classes
 	if (currentScroll > 0) {
 		navBar.classList.add("scroll-down");
 		hero.classList.add("scroll-down");
-        
-	} else if (currentScroll == 0) {
+	} 
+	// if it's equal to zero then remove the classes.
+	else if (currentScroll == 0) {
         navBar.classList.remove("scroll-down");
         hero.classList.remove("scroll-down");
         carousel.style.zIndex = 1;
