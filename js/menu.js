cee83f9e9c16c9e7886ed531426f902abfba5059
Ushen Wijayaratne <108585387+its-ushen@users.noreply.github.com>
2023-07-17 09:33:57 +0100

    Update menu.js
    commented the file

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,15 @@
+// getting all the elements from the html 
+
 const hamburger = document.querySelector('.hamburger_image');
 const navMenu = document.querySelector('.nav__menu');
 const navBar = document.querySelector('.nav__bar');
 const carousel = document.querySelector('.carousel');
 
-console.log(navBar);
+// Adding an event listener on the hamburger image. If it is clicked, then it will check 
+    // if the image is the menu image. 
+        // if so then set classes to active and change the image to the close image
+    // if the image is the close image 
+        // then set it back to the hamburger image and toggle off all the active classes. 
 
 hamburger.addEventListener('click', () => {
     if (hamburger.getAttribute('src').includes('menu.svg')) {
@@ -20,6 +26,7 @@ hamburger.addEventListener('click', () => {
     }
 });
 
+// if you click on a navigation element it would close the menu and remove all the toggled active tags, and then return back to the menu image.
 document.querySelectorAll('.nav__item').forEach(n => {
     n.addEventListener('click', () => {
         hamburger.setAttribute('src', 'Images/menu.svg');
